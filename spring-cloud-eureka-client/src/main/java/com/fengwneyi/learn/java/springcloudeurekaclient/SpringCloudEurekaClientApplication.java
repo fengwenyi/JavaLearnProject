466148921da81e6d0d62213f8fd7480bddee9618
Wenyi Feng <xfsy_2015@163.com>
2018-06-23 13:58:42 +0800

    Spring Cloud Eureka

---
+++ spring-cloud-eureka-client/src/main/java/com/fengwneyi/learn/java/springcloudeurekaclient/SpringCloudEurekaClientApplication.java
@@ -0,0 +1,14 @@
+package com.fengwneyi.learn.java.springcloudeurekaclient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+public class SpringCloudEurekaClientApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringCloudEurekaClientApplication.class, args);
+    }
+}
